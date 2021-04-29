b623d0f6ed14a48f2dc8676d716d4562a47dc82d
Stephen Meriwether <smeriwether@spotify.com>
2021-04-29 09:28:42 -0400

    Remove Wordpress and start over with a "Coming soon" page

--- deploy.sh
+++ deploy.sh
@@ -2,24 +2,15 @@
 
 echo "Building..."
 
-cd theme; zip -r -q -1 -X ../theme.zip *; cd ..
-
 echo "Deploying..."
 
-scp -o StrictHostKeyChecking=no -rp ./theme.zip $WEBSITE_SERVER:/tmp/theme.zip
+scp -o StrictHostKeyChecking=no -rp ./index.html $WEBSITE_SERVER:/var/www/merimerimeri.com/index.html
 if [ $? == 1 ]; then
   echo "Deploy failed"
   exit 1
 fi
 
 scp -o StrictHostKeyChecking=no -rp ./nginx.conf $WEBSITE_SERVER:/etc/nginx/sites-enabled/merimerimeri.com
-if [ $? == 1 ]; then
-  echo "Deploy failed"
-  exit 1
-fi
-
-ssh $WEBSITE_SERVER "rm -rf /var/www/merimerimeri.com/wp-content/themes/theme/"
-ssh $WEBSITE_SERVER "wp theme install /tmp/theme.zip --path=/var/www/merimerimeri.com --activate --allow-root"
 if [ $? == 1 ]; then
   echo "Deploy failed"
   exit 1
@@ -32,17 +23,5 @@ if [ $? == 1 ]; then
 fi
 
 echo "Cleaning up..."
-
-rm theme.zip
-if [ $? == 1 ]; then
-  echo "Deploy failed"
-  exit 1
-fi
-
-ssh $WEBSITE_SERVER "rm /tmp/theme.zip"
-if [ $? == 1 ]; then
-  echo "Deploy failed"
-  exit 1
-fi
 
 echo "Finished!"
